af079765f3639d991b656074b6220f0c9114962d
Milan Tol <milantol35@gmail.com>
2026-03-04 14:51:30 +0100

    .

--- Code_assignment/Agent.py
+++ Code_assignment/Agent.py
@@ -21,36 +21,31 @@ class BaseAgent:
     def select_action(self, s, policy='egreedy', epsilon=None, temp=None):
         
         if policy == 'greedy':
-            # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+            return argmax(self.Q_sa[s]) # follow "best action" according to current Q_sa
             
         elif policy == 'egreedy':
             if epsilon is None:
                 raise KeyError("Provide an epsilon")
                 
             if np.random.uniform(0, 1) < epsilon: #with probability epsilon, try a random action
-                a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+                return np.random.randint(0,self.n_actions) # randomly select an action
             else:
-                a = argmax(self.Q_sa[s]) #follow "best action" according to current Q_sa
+                return argmax(self.Q_sa[s]) #follow "best action" according to current Q_sa
                  
         elif policy == 'softmax':
             if temp is None:
                 raise KeyError("Provide a temperature")
-            
-            temp_inv = 1/temp #divide first, for computational speed
-            # when looking at eq (3), note that the denominator is nothing but normalization.
-            
-            epsilon = np.sum(self.Q_sa[s]*temp_inv) * np.random.uniform(0,1)
-            cumulative = np.cumsum(self.Q_sa[s]*temp_inv) #compute cumulative distribution
-            a = argmax(-np.abs(epsilon - cumulative)) #find root of epsilon - cumulative 
-              
-        return a
-        
+            #reduce Q to avoid infinities when exponentiating, this effectively divides everything by exp(Q_max/temp)
+            Q_reduced = self.Q_sa[s] - np.max(self.Q_sa[s]) 
+            exp_vals = np.exp(Q_reduced/temp)
+            probs =  exp_vals / np.sum(exp_vals)        
+            return np.random.choice(self.n_actions, p=probs) #selects numbers 0 to n_actions proportional to probs
+
     def update(self):
         raise NotImplementedError('For each agent you need to implement its specific back-up method') # Leave this and overwrite in subclasses in other files
 
 
-    def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
+    def evaluate(self, eval_env, n_eval_episodes=30, max_episode_length=100):
         returns = []  # list to store the reward per episode
         for i in range(n_eval_episodes):
             s = eval_env.reset()
--- Code_assignment/Q_learning.py
+++ Code_assignment/Q_learning.py
@@ -10,10 +10,13 @@ import numpy as np
 from Environment import StochasticWindyGridworld
 from Agent import BaseAgent
 
+import matplotlib.pyplot as plt
+
 class QLearningAgent(BaseAgent):
         
-    def update(self,s,a,r,s_next,done):
-        # TO DO: Add own code
+    def update(self, s, a, r, s_next, done):
+        G = r + self.gamma * np.max(self.Q_sa[s_next])
+        self.Q_sa[s,a] = self.Q_sa[s,a] + self.learning_rate * (G - self.Q_sa[s,a])
         pass
 
 def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
@@ -25,32 +28,101 @@ def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None
     agent = QLearningAgent(env.n_states, env.n_actions, learning_rate, gamma)
     eval_timesteps = []
     eval_returns = []
-    
-    # TO DO: Write your Q-learning algorithm here!
-    
-    # if plot:
-    #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during Q-learning execution
 
+    s = env._location_to_state(env.start_location)
+    t=0
+    while t < n_timesteps:
+        t += 1 
+        a = agent.select_action(s, policy=policy, epsilon=epsilon, temp=temp) #selection action based on some randomness policy
+        s_next, r, done = env.step(a) # perform action in environment to observe next state, gained reward, and termination condition
+        agent.update(s, a, r, s_next, done) #update Q, based on observed reward
+        if done: #if the run is done, start over.
+            s = env.reset()
+        else:
+            s = s_next
+
+        if t % eval_interval == 0:
+            eval_timesteps.append(t)
+            eval = agent.evaluate(eval_env)
+            eval_returns.append(eval) #use evaluation environment to not affect the testing environment!
+
+            if plot:
+                env.render(Q_sa=agent.Q_sa, plot_optimal_policy=True, step_pause=0.0001) # Plot the Q-value estimates during Q-learning execution   
 
     return np.array(eval_returns), np.array(eval_timesteps)   
 
-def test():
+def experiment(
+        policy = 'egreedy', epsilon=None, temp=None, gamma = 1, 
+        learning_rate=0.1,eval_interval = 1000, n_timesteps = 50001,
+        plot = False
+        ):
     
-    n_timesteps = 1000
-    eval_interval=100
-    gamma = 1.0
-    learning_rate = 0.1
+    n_timesteps = n_timesteps
+    eval_interval= eval_interval
+    gamma = gamma
+    learning_rate = learning_rate
 
     # Exploration
-    policy = 'egreedy' # 'egreedy' or 'softmax' 
-    epsilon = 0.1
-    temp = 1.0
+    policy = policy # 'egreedy' or 'softmax' 
+    epsilon = epsilon
+    temp = temp
     
     # Plotting parameters
-    plot = True
-
+    plot = plot
     eval_returns, eval_timesteps = q_learning(n_timesteps, learning_rate, gamma, policy, epsilon, temp, plot, eval_interval)
-    print(eval_returns,eval_timesteps)
+    return eval_returns, eval_timesteps
+
+
 
 if __name__ == '__main__':
-    test()
+
+    repetitions = 20
+
+    for epsilon in [0.03, 0.1, 0.3]:
+        eval_returns_list = []
+        for i in range(repetitions):
+            eval_returns, eval_timesteps = experiment(policy='egreedy', epsilon=epsilon, n_timesteps=50001, eval_interval=1000)
+            eval_returns_list.append(eval_returns)
+
+        mean_returns = np.mean(eval_returns_list, axis=0)
+        std_returns = np.std(eval_returns_list, axis=0)
+        stderr = std_returns / np.sqrt(repetitions)
+        ci95 = 1.96 * stderr
+
+        plt.plot(eval_timesteps, np.mean(eval_returns_list, axis=0), label='egreedy:' + r'$\epsilon=$' + f'{epsilon}')
+        plt.fill_between(
+            eval_timesteps[:len(mean_returns)],
+            mean_returns - ci95,
+            mean_returns + ci95,
+            alpha=0.25
+        )
+
+    plt.legend()
+    plt.savefig("/home/milan/Desktop/DRL/Assignment1/plots/Q_learning_egreedy.pdf")
+    plt.close()
+
+    for temp in [0.01, 0.1, 1]:
+
+        eval_returns_list = []
+        for i in range(repetitions):
+            eval_returns, eval_timesteps = experiment(policy='softmax', temp=temp, n_timesteps=50001, eval_interval=1000)
+            eval_returns_list.append(eval_returns)
+
+        mean_returns = np.mean(eval_returns_list, axis=0)
+        std_returns = np.std(eval_returns_list, axis=0)
+        stderr = std_returns / np.sqrt(repetitions)
+        ci95 = 1.96 * stderr
+
+        plt.plot(eval_timesteps, mean_returns, label='softmax:' + r'$T=$' + f'{temp}')
+        plt.fill_between(
+            eval_timesteps[:len(mean_returns)],
+            mean_returns - ci95,
+            mean_returns + ci95,
+            alpha=0.25
+        )
+
+    plt.legend()
+    plt.savefig("/home/milan/Desktop/DRL/Assignment1/plots/Q_learning_softmax.pdf")
+
+    plt.xlabel('episode')
+    plt.close()
