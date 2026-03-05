4385953d361e5e64f46ed7d5f04db9863a16dea8
Milan Tol <milantol35@gmail.com>
2026-03-05 11:24:48 +0100

    .

--- Code_assignment/Nstep.py
+++ Code_assignment/Nstep.py
@@ -11,15 +11,13 @@ from Environment import StochasticWindyGridworld
 from Agent import BaseAgent
 
 class NstepQLearningAgent(BaseAgent):
-        
-    def update(self, states, actions, rewards, done, n):
-        ''' 
-        states is a list of states observed in the episode, of length T_ep + 1 (last state is appended)
-        actions is a list of actions observed in the episode, of length T_ep
-        rewards is a list of rewards observed in the episode, of length T_ep
-        done indicates whether the final s in states is was a terminal state 
-        '''
-        gammas = self.gamma**np.arange(len(rewards)) #create array containing [gamma^0, gamma^1, ..., gamma^{n-1}]
+    
+    def _single_update(self, states, actions, rewards, done):
+        """
+        updates Q_sa for a single state and action, using Nstep. 
+        """
+        n = len(actions) # store total number of actions taken
+        gammas = self.gamma**np.arange(n) #create array containing [gamma^0, gamma^1, ..., gamma^{n-1}]
         sum_term = np.sum(gammas*rewards) #compute estimated total reward for current policy up till t + (n-1)
 
         #if final state is terminal, dont add estimated future reward after final state
@@ -33,7 +31,18 @@ class NstepQLearningAgent(BaseAgent):
             self.learning_rate * (target - self.Q_sa[states[0], actions[0]])
             )
         return 
-
+    
+    def update(self, states, actions, rewards, done, T_ep):
+        ''' 
+        states is a list of states observed in the episode, of length T_ep + 1 (last state is appended)
+        actions is a list of actions observed in the episode, of length T_ep
+        rewards is a list of rewards observed in the episode, of length T_ep
+        done indicates whether the final s in states is was a terminal state 
+        '''
+        for T in range(T_ep):
+            self._single_update(states[T:], actions[T:], rewards[T:], done=done) #state always contains 1 more element
+        return 
+           
 
 def n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma, 
                 policy='egreedy', epsilon=None, temp=None, plot=True, n=5, eval_interval=500):
@@ -57,39 +66,32 @@ def n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma,
         rewards = []
         #collect episode
         for T in range(max_episode_length):
-            a = agent.select_action(s)
-            r, s, done = env.step(a) 
+            a = agent.select_action(s, policy, epsilon=epsilon, temp=temp)
+            #overwrite state s 
+            s, r, done = env.step(a) 
             states.append(s)
             actions.append(a)
             rewards.append(r)
-            #overwrite state s 
             if done: #check whether agent reached terminal state, then end episode
                 break
         T_ep = T 
-
-        for T in range(T_ep):
-            if T_ep - T > target_depth: #if number of states left in episode is greater than target depth
-                m = target_depth
-                agent.update(states[:m], actions[:m], rewards[:m], done=False) # set done=False! 
-            else:
-                m = T_ep - T
-                agent.update(states[:m], actions[:m], rewards[:m], done=done) #set done = done
-
+        agent.update(states, actions, rewards, done=done, T_ep=T_ep)
+            
+        if plot:
+            env.render(Q_sa=agent.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during n-step Q-learning execution
+            
         if t % eval_interval == 0:
             eval_timesteps.append(t)
             eval = agent.evaluate(eval_env)
             eval_returns.append(eval) #use evaluation environment to not affect the testing environment!
 
-        if plot:
-            env.render(Q_sa=agent.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during n-step Q-learning execution
-            
     return np.array(eval_returns), np.array(eval_timesteps) 
 
 def test():
     n_timesteps = 10000
     max_episode_length = 100
     gamma = 1.0
-    learning_rate = 0.1
+    learning_rate = 0.3
     n = 5
     
     # Exploration
@@ -99,7 +101,7 @@ def test():
     
     # Plotting parameters
     plot = True
-    n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma, 
+    n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma,  
                    policy, epsilon, temp, plot, n=n)
     
     
